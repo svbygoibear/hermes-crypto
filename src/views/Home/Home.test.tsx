eba3a26380d024a308589a1d58feeccbb4ab9bcf
svbygoibear <svbyogibear@gmail.com>
2024-08-08 16:26:33 +0200

    💥 Fixed changes for build.

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import * as ue from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
