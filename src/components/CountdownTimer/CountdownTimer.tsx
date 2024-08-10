f9d03c8db3f8b9e078f2aa5eda954252061bdac8
svbygoibear <svbyogibear@gmail.com>
2024-08-10 16:32:42 +0200

    Fixed the AppHeader in smaller view mode

--- src/components/CountdownTimer/CountdownTimer.tsx
+++ src/components/CountdownTimer/CountdownTimer.tsx
@@ -11,7 +11,7 @@ export interface CountdownProps {
 }
 
 export const Countdown: React.FunctionComponent<CountdownProps> = (props: CountdownProps) => {
-    const [timerStartTime, setTimerStartTime] = useState<number>(0);
+    const [timerStart, setTimerStart] = useState<number>(0);
     const [timerTime, setTimerTime] = useState<number>(props.countdownTimeInSeconds * 1000);
 
     const timerRef = useRef<NodeJS.Timeout | null>(null);
@@ -34,7 +34,7 @@ export const Countdown: React.FunctionComponent<CountdownProps> = (props: Countd
         if (timerRef.current) {
             clearInterval(timerRef.current);
         }
-        setTimerStartTime(timerTime);
+        setTimerStart(timerTime);
 
         timerRef.current = setInterval(() => {
             setTimerTime(prevTime => {
@@ -47,28 +47,26 @@ export const Countdown: React.FunctionComponent<CountdownProps> = (props: Countd
                     }
                     // Alert the parent component that the countdown is complete
                     props.onCountdownComplete();
-                    setTimerStartTime(0);
+                    setTimerStart(0);
                     return props.countdownTimeInSeconds * 1000;
                 }
             });
         }, 10);
     };
 
-    const seconds = parseInt(("0" + (Math.floor((timerTime / 1000) % 60) % 60)).slice(-2));
-    console.log("These are the seconds=", seconds);
-    const minutes = Math.floor(timerTime / 60000);
-    console.log("These are the minutes=", minutes);
+    const totalSeconds = Math.floor(timerTime / 1000);
+    const minutes = totalSeconds === 60 ? 0 : Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds === 60 ? totalSeconds : totalSeconds % 60;
+
+    const isHalfway = timerTime > timerStart / 2 && timerTime > 1000;
+    const isTimesUp = timerStart !== 0 && timerTime <= 1000;
 
     return (
         <Container maxWidth="sm">
             <div className="Countdown-time">
-                {timerTime > timerStartTime / 2 && timerTime > 1000 && (
-                    <h3 className="countdowntimer-countdownAlert">HALFWAY THERE!</h3>
-                )}
+                {isHalfway && <h3 className="countdowntimer-countdownAlert">HALFWAY THERE!</h3>}
 
-                {timerStartTime !== 0 && timerTime <= 1000 && (
-                    <h3 className="countdowntimer-timesUp">TIME&apos;S UP!</h3>
-                )}
+                {isTimesUp && <h3 className="countdowntimer-timesUp">TIME&apos;S UP!</h3>}
 
                 <div className="countdowntimer-clockSim">
                     <FlipClock countMinutes={minutes} countSeconds={seconds} />
