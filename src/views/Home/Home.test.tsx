09ab09c79a6baed59a209fd15b3b32655140eaa2
svbygoibear <svbyogibear@gmail.com>
2024-08-13 00:15:49 +0200

    Fixed up some build issues.

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import * as ue from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import { Home } from "./Home";
 import "@testing-library/jest-dom";
@@ -7,14 +6,6 @@ import "@testing-library/jest-dom";
 describe("Simple Home Render Test", () => {
     it("the title is visible", () => {
         render(<Home isLoggedIn />);
-        expect(screen.getByText("HermesCrypto + React")).toBeInTheDocument();
-    });
-
-    it("should increment count on click", async () => {
-        render(<Home isLoggedIn />);
-        const counter = screen.getByRole("button", { name: /count is/i });
-        expect(counter.textContent).toBe("count is 0");
-        await ue.userEvent.click(counter);
-        expect(counter.textContent).toBe("count is 1");
+        expect(screen.getByText("Hermes-Crypto")).toBeInTheDocument();
     });
 });
